6aaae4a26a5becf896157f8060b0e7b26374ddab
Cothrax <clarklee.me@gmail.com>
2016-08-21 22:53:03 +0800

    Site updated: 2016-08-21 22:53:01

--- 2016/08/21/Bash对拍和数据生成/check.sh
+++ 2016/08/21/Bash对拍和数据生成/check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-name="test"	                    	#文件名
+name="test"                         #程序名
 
-for ((;1;))		                    #死循环
+for ((;1;))                         #死循环
 do
-    ./${name}_gen		            #数据生成器
-    ./${name}_che	            	#暴力程序
-    ./${name}		                #程序
+    ./${name}_gen                   #数据生成器
+    ./${name}_che                   #暴力程序
+    ./${name}                       #程序
     diff ${name}.out ${name}.ans    #比较
     if [ $? -ne "0" ]; then         #如果diff的输出$?和"0"不等
         echo "fail"
-        exit		            	#退出
+        exit                        #退出
     else
         echo "pass"
     fi
